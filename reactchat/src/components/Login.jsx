be7ff7ffd1029e1314e9d80e37ea9388781e084d
Manuel Casado <ManuelCasado@protonmail.com>
2022-04-06 13:48:36 +0200

    Next Level. PushDataSetter. 1º stage completed.

--- reactchat/src/components/Login.jsx
+++ reactchat/src/components/Login.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 const Login = ()=>{
 
     const [ user, keepUser ] = useState("");
     const [ pass, keepPass ] = useState("");
-    // const [ id, setId ] = useState ("");
+    const [ id, setId ] = useState ("");
 
     function getUserDataButtonHandler(){
         const urlLogin="https://web-develop-react-express-chat.herokuapp.com/login/"
         let userData=JSON.stringify({userName: user, password: pass});
         console.log("Enviando datos de alta de usuario...")
-        console.log(user + " , " +  pass);
+        //console.log(user + " , " +  pass);
         setUserPost(urlLogin, userData);
-        //return pass;
-    }
+        //keepUser("");
+        //keepPass("");
+   }
 
     function setUser(event){
         keepUser(event.target.value);
@@ -37,19 +38,30 @@ const Login = ()=>{
             }
         );
         const responseData = await response.json();
-        console.log(responseData);
-        //setId = responseData;
-        return responseData;
+        //console.log(responseData);
+        setId(responseData);
     }
+    
+    useEffect(
+        ()=>{
+          if (id === ""){ }
+          else {
+                console.log(`user: ${user}`);
+                console.log(`password: ${pass}`);
+                console.log(`id: ${id}`);
+                keepUser("");
+                keepPass("");
+        }}, [id]
+      )
 
     return (
 
         <div className="container">
             <h1>Alta Usuario</h1>
             <label htmlFor="user">Usuario:</label>
-            <input type="text" className='input' name="user" onChange={setUser}></input>
+            <input type="text" className='input' name="user" value={user} onChange={setUser}></input>
             <label htmlFor="pass">Password:</label>
-            <input type="password" className='input' name="pass" onChange={setPass}></input><br></br>
+            <input type="password" className='input' name="pass" value={pass} onChange={setPass}></input><br></br>
             <button type="button" className='button' onClick={getUserDataButtonHandler}>Enviar</button>
         </div>
 
