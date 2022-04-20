87053957e43e23158e168ff88e0c471590bf82bb
Manuel Casado <ManuelCasado@protonmail.com>
2022-04-20 09:04:32 +0200

    Cambio de directorio Proyecto ReactChatOficial

---
+++ ReactExpressChat_Oficial/src/views/LoginView.jsx
@@ -0,0 +1,14 @@
+import Login from "../components/Login";
+
+
+function LoginView () {
+    return (
+        <>
+            <h1>Login</h1>
+            <Login/>
+            
+        </>
+    )
+}
+
+export default LoginView
